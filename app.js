84923a5c474e54dc7fc2a41d05f2a0d9cb272eed
mtisby <marieltisby1998@gmail.com>
2021-11-22 13:05:09 -0800

    styling

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ import { User } from './models/user.js'
 import { gallery } from "./routes/gallery.js"
 import { userRoutes } from "./routes/users.js"
 
-const dbUrl = 'mongodb://localhost:27017/paint-app' || process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/paint-app';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
